f08beac397bd399493d85def925e2ad7ffd54d98
Hbsilva <heitorborges1@gmail.com>
2022-05-19 22:51:22 -0300

    ajustando a suit home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,11 +2,7 @@
 
 describe('home page', ()=>{
     it('app deve estar online', ()=>{
-        cy.viewport(1440, 900)
-        //comando utilizado para setar a resolução 
-        cy.visit('https://buger-eats.vercel.app')
-        //comando utilizado para acessar a página desejada
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
-        //comando utilizado para realizar uma verificação de texto
     })
 })
